367901ae7e33bf4d85a7675976648abb24576bbd
Nikolaj <nikolajbarnev@yandex.ru>
2023-07-12 13:34:08 +0300

    development(base) app

--- app/exchange_app/views.py
+++ app/exchange_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def exchange(request):
+    response = requests.get(url='https://api.exchangerate-api.com/v4/latest/USD').json()
+    currencies = response.get('rates')
+
+    if request.method == 'GET':
+        context = {
+            'currencies': currencies
+        }
+    return render(request=request, template_name='exchange_app/index.html', context=context)
