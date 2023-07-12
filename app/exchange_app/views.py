ba6e468f4d5da2797953e726b9899c09c473d97e
Nikolaj <nikolajbarnev@yandex.ru>
2023-07-12 20:04:23 +0300

    app done

--- app/exchange_app/views.py
+++ app/exchange_app/views.py
@@ -9,4 +9,20 @@ def exchange(request):
         context = {
             'currencies': currencies
         }
-    return render(request=request, template_name='exchange_app/index.html', context=context)
+        return render(request=request, template_name='exchange_app/index.html', context=context)
+
+    if request.method == 'POST':
+        from_amount = request.POST.get('from-amount')
+        from_curr = request.POST.get('from-curr')
+        to_curr = request.POST.get('to-curr')
+
+        converted_amount = round((currencies[to_curr] / currencies[from_curr]) * float(from_amount), 2)
+
+        context = {
+            'from_curr': from_curr,
+            'to_curr': to_curr,
+            'from_amount': from_amount,
+            'currencies': currencies,
+            'converted_amount': converted_amount
+        }
+        return render(request=request, template_name='exchange_app/index.html', context=context)
